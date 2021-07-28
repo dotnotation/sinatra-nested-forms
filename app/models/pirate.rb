2ec21d69edf7085c63e9f6a8adb4e5bf03758c99
dotnotation <dotinmotionproductions@gmail.com>
2021-07-28 17:19:58 -0400

    commit tree 0d45b4ced6095f3b43ad48442a886c9ebf42dfd3

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,19 @@
 class Pirate
+    attr_reader :name, :weight, :height
+
+    @@all = []
+
+    def initialize(params)
+        @name = params[:name]
+        @weight = params[:weight]
+        @height = params[:height]
+    end
+
+    def save
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
 end
