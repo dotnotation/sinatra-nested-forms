2e763eb1f3485a5bb2b41eb94b342f6d2047c90b
dotnotation <dotinmotionproductions@gmail.com>
2021-07-28 19:39:40 -0400

    adding database

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,20 @@
-class Pirate
+class Pirate < ActiveRecord::Base 
+    has_many :ships
+    attr_reader :name, :weight, :height
+
+    @@all = []
+
+    def initialize(params)
+        @name = params[:name]
+        @weight = params[:weight]
+        @height = params[:height]
+    end
+
+    def save
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,25 @@
-class Ship
+class Ship < ActiveRecord::Base
+    belongs_to :pirate
+    attr_reader :name, :type, :booty
+
+    SHIPS = []
+
+    def initialize(params)
+        @name = params[:name]
+        @type = params[:type]
+        @booty = params[:booty]
+        SHIPS << self
+    end
+
+    # def save
+    #     SHIPS << self
+    # end
+
+    def self.all
+        SHIPS
+    end
+
+    def self.clear
+        SHIPS.clear
+    end
 end
